b6014b5be1c65e0c18de6a3dd6e6e0f2c7396d59
tarurata <test@test.com>
2018-10-15 14:48:25 +0000

    add error hundling

--- railsinstall.sh
+++ railsinstall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -e
+#trap 'echo ERROR: line no = ${LINENO}, exit status = ${?} ;exit 1'
 
 # make install directory
-INSTALL_DIR='${HOME}/app/ror/sample1/'
+INSTALL_DIR="${HOME}/app/ror/sample1/"
 mkdir -p "${INSTALL_DIR}"
-cd "${INSTALL_DIR}"
 
 # Initialize bundle 
+cd "${INSTALL_DIR}"
 bundle init
 
 # Remove # at "rails" from Gemfile
@@ -14,6 +16,7 @@ sed -i 's/# gem "rails"/gem "rails", "~> 5.1.0"/g' "${INSTALL_DIR}Gemfile"
 # Install gem 
 # vender -> usually put 3rd party library.
 # vendor/bundlerは絶対パス必要かも・・要確認
+cd "${INSTALL_DIR}"
 bundle install --path vendor/bundler
 
 bundle exec rails new "${INSTALL_DIR}"
