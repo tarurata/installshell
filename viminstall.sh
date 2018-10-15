6350c9f725847ef0a0bc2515163417696ab839c4
tarurata <test@test.com>
2018-10-15 15:40:09 +0000

    complete viminstall

--- viminstall.sh
+++ viminstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
+set -e         
 
 yum install -y vim
-
+  
 git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
-
-wget https://raw.githubusercontent.com/tarurata/vimrc/master/.vimrc "${HOME}"
+    
+wget https://raw.githubusercontent.com/tarurata/vimrc/master/.vimrc -P "${HOME}"
 
 vim +PluginInstall +qall
