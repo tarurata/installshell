e01dd042c7b1064808c90447034eb1810444f2f6
tarurata <saycheese5963@gmail.com>
2022-05-12 22:30:12 +0900

    Update viminstall.sh

--- viminstall.sh
+++ viminstall.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# set -e         
+# set -e
 
-# Linux
-sudo apt-get install -y vim git wget curl
-# Mac
-## sudo brew install -y vim git wget curl
-  
+if [ uname -eq "Linux" ]; then
+  sudo apt-get install -y vim git wget curl
+elif [ uname -eq "Darwin" ]; then
+  sudo brew install -y vim git wget curl
+fi
+    
 # Get neccessary files and dirs for plugin.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     
-# Linux
-wget https://raw.githubusercontent.com/tarurata/config/master/.vimrc-linux -P "${HOME}" -O ".vimrc"
-# Mac
-## wget https://raw.githubusercontent.com/tarurata/config/master/.vimrc-forMac -P "${HOME}" -O ".vimrc"
+if [ uname -eq "Linux" ]; then
+  wget https://raw.githubusercontent.com/tarurata/config/master/.vimrc-linux -P "${HOME}" -O ".vimrc"
+elif [ uname -eq "Darwin" ]; then
+  wget https://raw.githubusercontent.com/tarurata/config/master/.vimrc-forMac -P "${HOME}" -O ".vimrc"
+fi
 
 vim +PlugInstall
