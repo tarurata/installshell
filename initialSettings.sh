ce11abb43c663f49927e7e40afcae8f6350cc3e3
tarurata <saycheese5963@gmail.com>
2022-06-03 03:36:44 +0900

    Update initialSettings.sh

--- initialSettings.sh
+++ initialSettings.sh
@@ -3,9 +3,9 @@
 set -eu
 
 if [ "$(uname)" = "Linux" ]; then
-  sudo apt-get install -y vim git wget curl
+  sudo apt-get install -y vim git wget curl tmux
 elif [ "$(uname)" = "Darwin" ]; then
-  sudo brew install -y vim git wget curl
+  sudo brew install -y vim git wget curl tmux
 fi
 
 # Get neccessary files and dirs for plugin.
@@ -19,5 +19,7 @@ elif [ "$(uname)" = "Darwin" ]; then
   wget https://raw.githubusercontent.com/tarurata/config/master/.bashrc-forMac -P "${HOME}" -O ".bashrc"
   wget https://raw.githubusercontent.com/tarurata/config/master/.vimrc-forMac -P "${HOME}" -O ".vimrc"
 fi
+
+wget https://raw.githubusercontent.com/tarurata/config/master/.tmux.conf -P "${HOME}"
 
 vim -c PlugInstall -c q -c q
